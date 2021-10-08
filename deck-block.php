5f45bfd754b91d0dea838df94884c7b7eacfe6cf
David Dashifen Kees <dashifen@dashifen.com>
2021-10-08 06:42:20 -0400

    better autoloading

--- deck-block.php
+++ deck-block.php
@@ -4,18 +4,15 @@
  * Description: A WordPress Deck block, which contains between two and four Cards.
  * Author URI: mailto:dashifen@dashifen.com
  * Author: David Dashifen Kees
- *
- * @noinspection PhpIncludeInspection
+ * Version: 2.0.0
  */
 
 use Dashifen\Blocks\DeckBlock\DeckBlock;
 use Dashifen\WPHandler\Handlers\HandlerException;
 
-$autoloader = file_exists(dirname(ABSPATH) . '/deps/vendor/autoload.php')
-  ? dirname(ABSPATH) . '/deps/vendor/autoload.php'    // production location
-  : 'vendor/autoload.php';                            // development location
-
-require_once($autoloader);
+if (!class_exists('Dashifen\Blocks\DeckBlock\DeckBlock')) {
+  require_once 'vendor/autoload.php';
+}
 
 try {
   (function () {
